c6a6c14a595f6ab8fed1e15f37b2ad2e61f056fb
Piotr Kukielka <piotr.kukielka@gmail.com>
2012-12-01 17:35:09 +0100

    Simple entries viewer

---
+++ js/main.js
@@ -0,0 +1,23 @@
+var forksList;
+
+var options = {
+	item: 'fork-item',
+	plugins: [ [ 'fuzzySearch' ] ]
+};
+
+$.getJSON("data/forks.json", function(_forks) {
+	var i, length, _results;
+	forks = [];
+	for (i = 0, length = _forks.length; i < length; i += 1) {
+		if (_forks[i].homepage !== "https://github.com/blog/1303-github-game-off") {
+			forks.push({
+				full_name: '<a href="' + _forks[i].html_url + '">' + _forks[i].full_name + '</a>',
+				avatar_url: '<img src="' +  _forks[i].owner.avatar_url + '" height="80" width="80"/>',
+				homepage: '<a href="' + _forks[i].homepage + '">' + _forks[i].homepage + '</a>',
+				description: _forks[i].description
+			});
+		}
+	}
+	console.log(forks)
+	forksList = new List('forks-list', options, forks);
+});
